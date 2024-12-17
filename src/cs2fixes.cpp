bf9f4fcefb2b21df8b1225bc4382ccddac6ae389
小彩旗 <410525209@qq.com>
2024-12-17 00:28:54 +0000

    test3

--- src/cs2fixes.cpp
+++ src/cs2fixes.cpp
@@ -75,7 +75,7 @@ void Message(const char* msg, ...)
 	char buf[1024] = {};
 	V_vsnprintf(buf, sizeof(buf) - 1, msg, args);
 
-	ConColorMsg(Color(255, 0, 255, 255), "[CS2Fixes] %s", buf);
+	ConColorMsg(Color(255, 0, 255, 255), "[XCQ] %s", buf);
 
 	va_end(args);
 }
--- src/detours.cpp
+++ src/detours.cpp
@@ -317,10 +317,10 @@ void SayChatMessageWithTimer(IRecipientFilter& filter, const char* pText, CCSPla
 		int mins = iTriggerTime / 60;
 		int secs = iTriggerTime % 60;
 
-		V_snprintf(buf, sizeof(buf), "%s %s %s %2d:%02d", " \7CONSOLE:\4", pText + sizeof("Console:"), "\x10- @", mins, secs);
+		V_snprintf(buf, sizeof(buf), "%s %s %s %2d:%02d", " \x0B[地图提示]:\x06", pText + sizeof("Console:"), "\x10- @", mins, secs);
 	}
 	else
-		V_snprintf(buf, sizeof(buf), "%s %s", " \7CONSOLE:\4", pText + sizeof("Console:"));
+		V_snprintf(buf, sizeof(buf), "%s %s", " \x0B[地图提示]:\x06", pText + sizeof("Console:"));
 
 	UTIL_SayTextFilter(filter, buf, pPlayer, eMessageType);
 }
@@ -338,7 +338,7 @@ void FASTCALL Detour_UTIL_SayTextFilter(IRecipientFilter& filter, const char* pT
 		return SayChatMessageWithTimer(filter, pText, pPlayer, eMessageType);
 
 	char buf[256];
-	V_snprintf(buf, sizeof(buf), "%s %s", " \7CONSOLE:\4", pText + sizeof("Console:"));
+	V_snprintf(buf, sizeof(buf), "%s %s", " \x0B[地图提示]:\x06", pText + sizeof("Console:"));
 
 	UTIL_SayTextFilter(filter, buf, pPlayer, eMessageType);
 }
